4278a533f40249db985737c33c19581e3aadb55b
Philipp <philipp@redox-labs.com>
2017-04-01 23:29:35 +0200

    basic epxress setup

--- mcp/src/index.ts
+++ mcp/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require('express');
+const HTTP = require('http');
+const PORT = 8080; 
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('GET request to /')
+})
+
+app.listen(PORT, function () {
+  console.log('MCP server listening on port %s!', PORT);
+})
